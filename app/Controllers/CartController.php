bee33a7b0a400eb582cbb5613fc2b30dbc207ed7
RaidyMR <raidymustafid39@gmail.com>
2022-01-13 00:06:04 +0800

    bugfixes: hak akses cart

--- app/Controllers/CartController.php
+++ app/Controllers/CartController.php
@@ -9,18 +9,19 @@ class CartController extends BaseController {
     public function index() {
         $session = session();
         
-        $cartID = $this->CartsModel->getIdCartActiveByUserId($session->get('id'));
-        $quantity = $this->request->getVar('quantity', FILTER_SANITIZE_FULL_SPECIAL_CHARS);
-        $variantID = $this->request->getVar('variantID', FILTER_SANITIZE_FULL_SPECIAL_CHARS);
-        
-        $this->CartDetailsModel->addItemToCart($cartID, $quantity, $variantID);
-
-        if($session->get('id')){            
+        if($session->get('id')){    
+            $cartID = $this->CartsModel->getIdCartActiveByUserId($session->get('id'));        
+            $quantity = $this->request->getVar('quantity', FILTER_SANITIZE_FULL_SPECIAL_CHARS);
+            $variantID = $this->request->getVar('variantID', FILTER_SANITIZE_FULL_SPECIAL_CHARS);
+            
+            $this->CartDetailsModel->addItemToCart($cartID, $quantity, $variantID);
             # $session->setFlashdata('msg', 'Pemesanan Berhasil');
             
-            #ke home dlu, nanti klo udh jdi pageny perlu dirombak lagi
+            # ke home dlu, nanti klo udh jdi pageny perlu dirombak lagi
             return redirect()->to('/home'); 
-        }        
+        } else {
+            return redirect()->to('/login'); 
+        }
     }
 }
 
