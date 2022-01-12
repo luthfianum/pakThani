522e235641677a60bcef50a0a0fd345d615b1804
RaidyMR <raidymustafid39@gmail.com>
2022-01-13 00:03:57 +0800

    frontend: bantu2 homepage

--- app/Views/home.php
+++ app/Views/home.php
@@ -17,7 +17,10 @@
 $data = $this->data;
 $item = $data['items'];
 $random = $item['random'];
+$daging = $item['daging'];
+$buah = $item['buah'];
 ?>
+
 
 
 <?= $this->section('content'); ?>
@@ -29,52 +32,55 @@ $random = $item['random'];
     <div class="carousel" style="display:flex;">
         <div class="owl-carousel owl-theme" style="width:55%">
             <div class="item">
-                <img src="https://i.ibb.co/3RdkjFg/Animated-Carousel-2.png" alt="" style="width: 100%;max-height: 300px">
+                <img src="https://i.ibb.co/FK9DGjL/Property-1-Frame-1.png" alt="" style="width: 100%;max-height: 300px">
             </div>
             <div class="item">
-                <img src="https://i.ibb.co/k29tZGx/ubi-cilembu.png" alt="" style="width: 100%; max-height: 300px">
+                <img src="https://i.ibb.co/MR9gnFB/Property-1-Frame-2.png" alt="" style="width: 100%; max-height: 300px">
             </div>
             <div class="item">
-                <img src="https://i.ibb.co/P4by44B/telur-puyuh.png" alt="" style="width: 100%; max-height: 300px">
+                <img src="https://i.ibb.co/Jyx1QdV/Property-1-Frame-3.png" alt="" style="width: 100%; max-height: 300px">
+            </div>
+            <div class="item">
+                <img src="hhttps://i.ibb.co/3hQ1Tb8/Property-1-Frame-4.png" alt="" style="width: 100%; max-height: 300px">
             </div>
         </div>
-        <div class="kategori" style="width: 33.33%; display:flex; flex-wrap:wrap; align-items:center">
-            <div class="logoAtas" style="width: 30%; text-align:center; padding-top:30px;align-items:center ">
+        <div class="kategori" style="width: 33.33%; display:flex; flex-wrap:wrap; align-items:space-between">
+            <div class="logoAtas" style="width: 30%; text-align:center;align-items:center ">
                 <a href="" style="display: flex;flex-direction:column; align-items:center ">
                     <img src="https://i.ibb.co/D7jgSKx/icon-Thanksgiving.png" alt="" style="width: 50%; ">
                     Daging
                 </a>
             </div>
-            <div class="logoAtas" style="width: 30%; text-align:center; padding-top:30px;">
+            <div class="logoAtas" style="width: 30%; text-align:center;">
                 <a href="" style="display: flex;flex-direction:column; align-items:center ">
                     <img src="https://i.ibb.co/4R4w5K5/icon-Group-Of-Fruits.png" alt="" style="width: 50%;">
                     Buah
                 </a>
             </div>
-            <div class="logoAtas" style="width: 30%; text-align:center; padding-top:30px; ">
+            <div class="logoAtas" style="width: 30%; text-align:center; ">
                 <a href="" style="display: flex;flex-direction:column; align-items:center ">
                     <img src="https://i.ibb.co/88xHxQB/icon-Garlic.png" alt="" style="width: 50%;">
                     Rempah
                 </a>
 
             </div>
-            <div class="logoAtas" style="width: 30%; text-align:center; padding-top:30px; ">
+            <div class="logoAtas" style="width: 30%; text-align:center; ">
                 <a href="" style="display: flex;flex-direction:column; align-items:center ">
                     <img src="https://i.ibb.co/23BGj1r/icon-Group-Of-Vegetables.png" alt="" style="width: 50%;">
                     Sayuran
                 </a>
 
             </div>
-            <div class="logoAtas" style="width: 30%; text-align:center; padding-top:30px; ">
+            <div class="logoAtas" style="width: 30%; text-align:center; ">
                 <a href="" style="display: flex;flex-direction:column; align-items:center ">
-                    <img src="https://i.ibb.co/88xHxQB/icon-Garlic.png" alt="" style="width: 50%;">
+                    <img src="https://i.ibb.co/0VpWnxC/icon-Natural-Food.png" alt="" style="width: 50%;">
                     Organik
                 </a>
 
             </div>
-            <div class="logoAtas" style="width: 30%; text-align:center; padding-top:30px; ">
+            <div class="logoAtas" style="width: 30%; text-align:center; ">
                 <a href="" style="display: flex;flex-direction:column; align-items:center ">
-                    <img src="https://i.ibb.co/88xHxQB/icon-Garlic.png" alt="" style="width: 50%;">
+                    <img src="https://i.ibb.co/M5gHb45/icon-Bookmark.png" alt="" style="width: 50%;">
                     Terlaris
                 </a>
 
@@ -100,7 +106,8 @@ $random = $item['random'];
                 <div class="card">
                     <img class="card__image" src="<?= $item['image']; ?>" alt="">
                     <div class="card__content">
-                        <?= $item['description']; ?>
+                        <?= $item['name']; ?>
+                        <p><span id="price">Rp <?= $item['variants_item'][0]['price']; ?></span><span id="variant">/<?= $item['variants_item'][0]['name']; ?></span> </p>
                     </div>
                     <div class="beliContainer">
                         <button class="beliButton" onClick="updateData()">
@@ -123,12 +130,12 @@ $random = $item['random'];
             <a href="">Lihat Semua</a>
         </div>
         <div class="cards" style="display: flex; justify-content:space-around">
-            <?php for ($x = 0; $x < 6; $x++) : ?>
-
+            <?php foreach ($daging as $item) : ?>
                 <div class="card">
-                    <img class="card__image" src="https://fakeimg.pl/400x300/009578/fff/" alt="">
+                    <img class="card__image" src="<?= $item['image']; ?>" alt="">
                     <div class="card__content">
-
+                        <?= $item['name']; ?>
+                        <p>Rp <span id="price"><?= $item['variants_item'][0]['price']; ?></span> / <span id="variant"><?= $item['variants_item'][0]['name']; ?></span> </p>
                     </div>
                     <div class="beliContainer">
                         <button class="beliButton" onClick="updateData()">
@@ -136,31 +143,37 @@ $random = $item['random'];
                         </button>
                     </div>
                 </div>
-            <?php endfor; ?>
+            <?php endforeach; ?>
         </div>
     </div>
 
     <div class="carousel" style="display:flex; padding-top:20px; padding-right:5px">
         <div class="owl-carousel owl-theme" style="width:43.75%">
             <div class="item">
-                <img src="https://i.ibb.co/fSzsptW/baby-jagung.png" alt="" style="width: 100%;max-height: 300px">
+                <img src="https://i.ibb.co/Jyx1QdV/Property-1-Frame-3.png" alt="" style="width: 100%; max-height: 300px">
             </div>
             <div class="item">
-                <img src="https://i.ibb.co/k29tZGx/ubi-cilembu.png" alt="" style="width: 100%; max-height: 300px">
+                <img src="https://i.ibb.co/MR9gnFB/Property-1-Frame-2.png" alt="" style="width: 100%; max-height: 300px">
             </div>
             <div class="item">
-                <img src="https://i.ibb.co/P4by44B/telur-puyuh.png" alt="" style="width: 100%; max-height: 300px">
+                <img src="https://i.ibb.co/FK9DGjL/Property-1-Frame-1.png" alt="" style="width: 100%;max-height: 300px">
+            </div>
+            <div class="item">
+                <img src="hhttps://i.ibb.co/3hQ1Tb8/Property-1-Frame-4.png" alt="" style="width: 100%; max-height: 300px">
             </div>
         </div>
         <div class="owl-carousel owl-theme" style="width:43.75%">
             <div class="item">
-                <img src="https://i.ibb.co/fSzsptW/baby-jagung.png" alt="" style="width: 100%;max-height: 300px">
+                <img src="https://i.ibb.co/MR9gnFB/Property-1-Frame-2.png" alt="" style="width: 100%; max-height: 300px">
             </div>
             <div class="item">
-                <img src="https://i.ibb.co/k29tZGx/ubi-cilembu.png" alt="" style="width: 100%; max-height: 300px">
+                <img src="https://i.ibb.co/Jyx1QdV/Property-1-Frame-3.png" alt="" style="width: 100%; max-height: 300px">
             </div>
             <div class="item">
-                <img src="https://i.ibb.co/P4by44B/telur-puyuh.png" alt="" style="width: 100%; max-height: 300px">
+                <img src="hhttps://i.ibb.co/3hQ1Tb8/Property-1-Frame-4.png" alt="" style="width: 100%; max-height: 300px">
+            </div>
+            <div class="item">
+                <img src="https://i.ibb.co/FK9DGjL/Property-1-Frame-1.png" alt="" style="width: 100%;max-height: 300px">
             </div>
         </div>
     </div>
@@ -172,13 +185,12 @@ $random = $item['random'];
             <a href="">Lihat Semua</a>
         </div>
         <div class="cards " style="display: flex; justify-content:space-around">
-
-            <?php for ($x = 0; $x < 6; $x++) : ?>
-
+            <?php foreach ($buah as $item) : ?>
                 <div class="card">
-                    <img class="card__image" src="https://fakeimg.pl/400x300/009578/fff/" alt="">
+                    <img class="card__image" src="<?= $item['image']; ?>" alt="">
                     <div class="card__content">
-
+                        <?= $item['name']; ?>
+                        <p>Rp <span id="price"><?= $item['variants_item'][0]['price']; ?></span> / <span id="variant"><?= $item['variants_item'][0]['name']; ?></span> </p>
                     </div>
                     <div class="beliContainer">
                         <button class="beliButton" onClick="updateData()">
@@ -186,8 +198,7 @@ $random = $item['random'];
                         </button>
                     </div>
                 </div>
-            <?php endfor; ?>
-
+            <?php endforeach; ?>
         </div>
     </div>
 
