b8e74c22b34fe55eee1aeeec14fdcc12e1846372
RaidyMR <raidymustafid39@gmail.com>
2022-01-13 00:06:21 +0800

    improvisasi seeder variant

--- app/Database/Seeds/VariantsSeeder.php
+++ app/Database/Seeds/VariantsSeeder.php
@@ -8,32 +8,25 @@ class VariantsSeeder extends Seeder
 {
     public function run()
     {
-        $data = [
-            [
-                'id'            => 1,
-                'name'          => '250gr',
-                'price'         => '8000',
-                'item_id'       => 1,
-            ],
-            [
-                'id'            => 2,
-                'name'          => '500gr',
-                'price'         => '15000',
-                'item_id'       => 1,
-            ],
-            [
-                'id'            => 3,
-                'name'          => '500gr',
-                'price'         => '10000',
-                'item_id'       => 2,
-            ],
-            [
-                'id'            => 4,
-                'name'          => '1000gr',
-                'price'         => '20000',
-                'item_id'       => 2,
-            ]
-        ];
+        $data = [];
+        $id = 1;
+
+        for ($i = 1; $i <= 21; $i++){
+            $data[] =   [
+                            'id'            => $id,
+                            'name'          => '250gr',
+                            'price'         => '8000',
+                            'item_id'       => $i,
+                        ];
+
+            $data[] =   [
+                            'id'            => $id+1,
+                            'name'          => '250gr',
+                            'price'         => '8000',
+                            'item_id'       => $i,
+                        ];
+            $id += 2;
+        }
 
         $this->db->table('variants_item')->insertBatch($data);
     }
