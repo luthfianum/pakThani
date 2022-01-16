7a6ea91f3956d6ac21ecfda9b9e6831ff30b6d2d
Luthfi Anum Pratama <luthfianump@gmail.com>
2022-01-16 15:31:14 +0800

    add: checkout & listTransactionPage

--- app/Controllers/TransactionController.php
+++ app/Controllers/TransactionController.php
@@ -18,4 +18,52 @@ class TransactionController extends BaseController
       return redirect()->to(base_url() . '/login');
     }
   }
+
+  public function checkout()
+  {
+    $session = session();
+    $userId = $session->get('id');
+
+    if ($userId) {
+      $this->db->transBegin();
+      $paymentTypeId = $this->request->getVar('paymentTypeId', FILTER_SANITIZE_FULL_SPECIAL_CHARS);
+      $user = $this->UserModel->getDetailsById($userId);
+      $cartId = $user['cart']['id'];
+      $addressesId = $user['address']['id'];
+      $transaction = $this->TransactionsModel->createTransaction($userId, $cartId, $addressesId, $paymentTypeId);
+      
+      if ($this->db->transStatus() === false) {
+        $this->db->transRollback();
+        $session->setFlashdata('msg', 'Transaksi Gagal Dibuat');
+      } else {
+        $this->db->transCommit();
+        $session->setFlashdata('msg', 'Transaksi Berhasil Dibuat');
+      }
+      return redirect()->to(base_url() . '/transaction');
+    } else {
+      return redirect()->to(base_url() . '/login');
+    }
+  }
+
+  public function listTransactionPage()
+  {
+    $session = session();
+    $userId = $session->get('id');
+    $result = [];
+    if ($userId) {
+      $result['transactions'] = $this->TransactionsModel->getByUserId($userId);
+      $result['user'] = $this->UserModel->getById($session->get('id'));
+      $result['user']['cart'] = $this->CartsModel->getByUserId($session->get('id'));
+      foreach($result['transactions'] as $i=>$transaction){
+        $addressId = $result['transactions'][$i]['address_id'];
+        $cartId = $result['transactions'][$i]['cart_id'];
+        $result['transactions'][$i]['address'] = $this->AddressesModel->find($addressId);
+        $result['transactions'][$i]['cart'] = $this->CartsModel->getDetailsById($cartId);
+      }
+      dd($result);
+      return view('list_transaksi', $result);
+    } else {
+      return redirect()->to(base_url() . '/login');
+    }
+  }
 }
