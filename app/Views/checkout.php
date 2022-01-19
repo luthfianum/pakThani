a8484e0c8ccc1b77f7edcf42aec06fbf9c6b7456
hanifirvin <hanif.bro10@gmail.com>
2022-01-20 00:06:16 +0700

    frontend: integrasi checkout

--- app/Views/checkout.php
+++ app/Views/checkout.php
@@ -8,10 +8,11 @@
     <?= $this->renderSection('content'); ?>
     <title>Checkout</title>
 </head>
+
 <body>
     <div class="container1">
         <div class="checkout cf">
-            <a href="#">
+            <a href="<?php echo base_url(); ?>">
                 <img src="/assets/arrow.png">
                 <div>Checkout</div>
             </a>
@@ -33,19 +34,22 @@
                 <h2>Ringkasan Pembayaran</h2>
                 <div class="total">
                     <h3>Total Harga</h3>
-                    <h3>Rp 45.000</h3>
+                    <h3>Rp <span id="totalHarga"><?= $cart['total']; ?></span></h3>
                 </div>
                 <div class="total">
                     <h3>Total Ongkos Kirim</h3>
-                    <h3>Rp 2.000</h3>
+                    <h3>Rp <span id="totalOngkir"><?= $cart['total']; ?></span></h3>
                 </div>
                 <hr>
                 <div class="total">
                     <h3>Total Tagihan</h3>
-                    <h3>Rp 47.000</h3>
+                    <h3>Rp <span id="totalTagihan"></span></h3>
                 </div>
                 <div class="button">
-                    <button type="submit"><h3>Pilih Pembayaran</h3></button>
+                    <form action="<?= base_url(); ?>" method="post">
+                        <input type="hidden" name="totalHarga" value="<?= $cart['total']; ?>" id="qty">
+                        <input class="buttonBayar" type="submit" value="Pilih Pembayaran" >
+                    </form>
                 </div>
             </div>
         </div>
@@ -54,35 +58,42 @@
     <div class="container3">
         <h2>Detail Pesanan</h2>
         <div class="detail">
-            <div class="detailPesanan">
-                <div class="item cf">
-                    <img src="">
-                    <div>
-                        <h3>Daging Sapi Amazon</h3>
-                        <h3><span>Rp 45.000</span> / 1 kg</h3>
-                    </div>
-                </div>
-                <div class="jumlah cf">
-                    <div class="delete">
-                        <button type="button" class="danger">Delete</button>
-                    </div>
-                    <div class="plusminus">
-                        <a onclick="decrement()"><img src="/assets/minus.png"></a>
-                        <input id=demoInput type=number min=0 max=110>
-                        <a onclick="increment()"><img src="/assets/tambah.png"></a>
+            <?php foreach ($cart['cartDetails'] as $cartD) : ?>
+                <div class="detailPesanan">
+                        <div class="item cf">
+                            <img src="<?= $cartD['image']; ?>">
+                            <div>
+                                <h3><?= $cartD['item_name']; ?></h3>
+                                <h3><span id="cartitem_price"><?= $cartD['price']; ?></span><span id='slash'> / </span><span id="cartitem_variant"></span><?= $cartD['variant']; ?></h3>
+                            </div>
                         </div>
-                    <div>
+                        <div class="jumlah cf">
+                            <div class="delete">
+                                <button type="button" class="danger">Delete</button>
+                            </div>
+                            <div class="plusminus">
+                                <a onclick="decrement()"><img src="/assets/minus.png"></a>
+                                <input id=demoInput type=number min=0 max=110 value="<?= $cartD['quantity']; ?>">
+                                <a onclick="increment()"><img src="/assets/tambah.png"></a>
+                            </div>
+                        </div>
                 </div>
-            </div>
+            <?php endforeach; ?>
         </div>
     </div>
 
     <script>
+        hitungTagihan();
         function increment() {
             document.getElementById('demoInput').stepUp();
+
         }
         function decrement() {
             document.getElementById('demoInput').stepDown();
+        }
+
+        function hitungTagihan() {
+            document.getElementById('totalTagihan').innerHTML = <?= ($cart['total']+$cart['total']); ?>;
         }
     </script>
 </body>
