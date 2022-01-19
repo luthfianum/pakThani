3761f689ef119506eed9a53971811e8d0587267e
Nabilkarren <nabilgunner@gmail.com>
2022-01-20 00:44:44 +0700

    Frontend : Modal making in checkout

--- app/Views/checkout.php
+++ app/Views/checkout.php
@@ -1,15 +1,21 @@
 <!DOCTYPE html>
 <html lang="en">
+
+
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="/css/checkout.css">
-    <?= $this->renderSection('content'); ?>
+    <link rel="stylesheet" href="/css/modal.css">
+
     <title>Checkout</title>
 </head>
 
+
 <body>
+
     <div class="container1">
         <div class="checkout cf">
             <a href="<?php echo base_url(); ?>">
@@ -21,12 +27,34 @@
     <div class="container2">
         <div class="alamat cf">
             <h2>Alamat Pengiriman</h2>
-            <a href="#">Ubah Alamat</a>
+            <a id="btn-alamat" href="#" onclick="">Ubah Alamat</a>
+
+            <div id="modal-container" class="modal">
+
+                <div class="modal-content">
+
+                    <span class="close">&times;</span>
+
+                    <h2>Ubah Alamat</h2>
+
+                    <div class="input-group">
+                        <label for="address-name">Alamat</label>
+                        <input type="nama-alamat" id="nama-alamat" name="nama-alamat" placeholder="alamat">
+                    </div>
+
+                    <div class="input-group">
+                        <label for="address">Catatan</label>
+                        <input type="catatan" id="alamat" name="alamat" placeholder="catatan">
+                    </div>
+
+                </div>
+
+            </div>
+
             <div class="detailAlamat">
-                <h2>Rumah Utama</h2>
-                <p>nama</p>
-                <p>nomor</p>
-                <p>alamat</p>
+                <h2><span id="jenisLokasi"><?= $addresses[0]['note']; ?></span></h2>
+                <p><span id="nama"><?= $user['username']; ?></span></p>
+                <p><span id="alamat"><?= $addresses[0]['alamat']; ?></span></p>
             </div>
         </div>
         <div class="ringkasan">
@@ -46,9 +74,8 @@
                     <h3>Rp <span id="totalTagihan"></span></h3>
                 </div>
                 <div class="button">
-                    <form action="<?= base_url(); ?>" method="post">
-                        <input type="hidden" name="totalHarga" value="<?= $cart['total']; ?>" id="qty">
-                        <input class="buttonBayar" type="submit" value="Pilih Pembayaran" >
+                    <form action="<?= base_url(); ?>/checkout" method="post">
+                        <input class="buttonBayar" type="submit" value="Payment">
                     </form>
                 </div>
             </div>
@@ -60,41 +87,69 @@
         <div class="detail">
             <?php foreach ($cart['cartDetails'] as $cartD) : ?>
                 <div class="detailPesanan">
-                        <div class="item cf">
-                            <img src="<?= $cartD['image']; ?>">
-                            <div>
-                                <h3><?= $cartD['item_name']; ?></h3>
-                                <h3><span id="cartitem_price"><?= $cartD['price']; ?></span><span id='slash'> / </span><span id="cartitem_variant"></span><?= $cartD['variant']; ?></h3>
-                            </div>
+                    <div class="item cf">
+                        <img src="<?= $cartD['image']; ?>">
+                        <div>
+                            <h3><?= $cartD['item_name']; ?></h3>
+                            <h3><span id="cartitem_price"><?= $cartD['price']; ?></span><span id='slash'> / </span><span id="cartitem_variant"></span><?= $cartD['variant']; ?></h3>
                         </div>
-                        <div class="jumlah cf">
-                            <div class="delete">
-                                <button type="button" class="danger">Delete</button>
-                            </div>
-                            <div class="plusminus">
-                                <a onclick="decrement()"><img src="/assets/minus.png"></a>
-                                <input id=demoInput type=number min=0 max=110 value="<?= $cartD['quantity']; ?>">
-                                <a onclick="increment()"><img src="/assets/tambah.png"></a>
-                            </div>
+                    </div>
+                    <div class="jumlah cf">
+                        <div class="delete">
+                            <button type="button" class="danger">Delete</button>
                         </div>
+                        <div class="plusminus">
+                            <a onclick="decrement()"><img src="/assets/minus.png"></a>
+                            <input id=demoInput type=number min=0 max=110 value="<?= $cartD['quantity']; ?>">
+                            <a onclick="increment()"><img src="/assets/tambah.png"></a>
+                        </div>
+                    </div>
                 </div>
             <?php endforeach; ?>
-        </div>
-    </div>
 
-    <script>
-        hitungTagihan();
-        function increment() {
-            document.getElementById('demoInput').stepUp();
+            <script>
+                hitungTagihan();
 
-        }
-        function decrement() {
-            document.getElementById('demoInput').stepDown();
-        }
+                function increment() {
+                    document.getElementById('demoInput').stepUp();
 
-        function hitungTagihan() {
-            document.getElementById('totalTagihan').innerHTML = <?= ($cart['total']+$cart['total']); ?>;
-        }
-    </script>
+                }
+
+                function decrement() {
+                    document.getElementById('demoInput').stepDown();
+                }
+
+                function hitungTagihan() {
+                    document.getElementById('totalTagihan').innerHTML = <?= ($cart['total'] + $cart['total']); ?>;
+                }
+            </script>
+            <script>
+                function increment() {
+                    document.getElementById('demoInput').stepUp();
+                }
+
+                function decrement() {
+                    document.getElementById('demoInput').stepDown();
+                }
+
+                var modal = document.getElementById("modal-container");
+                var btn = document.getElementById("btn-alamat");
+                var span = document.getElementsByClassName("close")[0];
+
+                btn.onclick = function() {
+                    modal.style.display = "block";
+                }
+
+                span.onclick = function() {
+                    modal.style.display = "none";
+                }
+
+                window.onclick = function(event) {
+                    if (event.target == modal) {
+                        modal.style.display = "none";
+                    }
+                }
+            </script>
 </body>
+
 </html>
