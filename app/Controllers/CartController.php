fbcc517dcd65ec7c505f4fa2bca4ea3368162444
hanifirvin <hanif.bro10@gmail.com>
2022-01-20 01:57:21 +0700

    backend: bug fix cart controller

--- app/Controllers/CartController.php
+++ app/Controllers/CartController.php
@@ -8,7 +8,7 @@ use CodeIgniter\HTTP\IncomingRequest;
 class CartController extends BaseController {
     public function index() {
         if($this->session->get('id')){    
-            $cartID = $this->CartsModel->getIdCartActiveByUserId($session->get('id'));        
+            $cartID = $this->CartsModel->getIdCartActiveByUserId($this->session->get('id'));        
             $quantity = $this->request->getVar('quantity', FILTER_SANITIZE_FULL_SPECIAL_CHARS);
             $variantID = $this->request->getVar('variantID', FILTER_SANITIZE_FULL_SPECIAL_CHARS);
                   
